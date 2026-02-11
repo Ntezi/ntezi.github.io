51bff9afadde319ceac9f4aadd90ccac5a964fa9
Ntezi <ngabomarius@gmail.com>
2026-02-11 08:23:09 +0000

    Remove deprecated swcMinify config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
     output: "export",
     distDir: "out",
     reactStrictMode: true,
-    swcMinify: true,
     exportPathMap: async function () {
         return {
             '/': {page: '/'},
